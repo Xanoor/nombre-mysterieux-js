da93f66cec8e5fe692a4649c3b788cad57f6be60
Xanoor1 <62515103+Xanoor@users.noreply.github.com>
2020-07-31 22:22:24 +0200

    Add files via upload

---
+++ Game/Nombre Mustérieux.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client, message, args) => {
+
+
+    let number = Math.floor(Math.random() * Math.floor(100))
+
+
+    message.channel.send('Nombre Mystérieux - Cherche le nombre entre 0 et 100 en suivant les indications ! Credits: https://github.com/Xanoor/nombre-mysterieux').then(async m => {
+
+
+
+        const filter = m => m.author.id !== client.user.id;
+        const collector = await m.channel.createMessageCollector(filter, {
+            time: 600000
+        });
+
+        collector.on("collect", async collected => {
+            if (collected.content.toLowerCase() === "annuler") {
+                return collector.stop(`✅ Nombre Mystérieux annulé !`);
+            } else {
+                let response = await collected.content.trim();
+                response = parseInt(response);
+                if (isNaN(response)) {
+                    return message.channel.send("⚠ Ce n'est pas un nombre !").then(msg => {
+                        msg.delete({
+                            timeout: 2000
+                        })
+                    })
+                }
+                if (response == number) {
+                    await collector.stop(
+                        `${collected.author.toString()} bravo ! Le nombre était: **${number}**`
+                    );
+                }
+                if (response > number) {
+                    message.channel.send("Le nombre est plus petit !")
+                }
+                if (response < number) {
+                    message.channel.send("Le nombre est plus grand !")
+                }
+            }
+        })
+        collector.on("end", async(collected, reason) => {
+            if (reason && reason !== "time") {
+                return message.channel.send(reason);
+            } else {
+                return message.channel.send(
+                    `Personne a remporté le nombre mystérieux, le nombre était: **${number}**`
+                );
+            }
+        });
+    })
+}
+
+module.exports.cmd = {
+    name: 'nbm'
+}
